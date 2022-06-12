9756e1d2d7fe434a81b4081728f935560a399b67
Lam Hai Trieu <thanhbuanham36@gmail.com>
2022-06-12 19:51:32 +0700

    xem lịch lam việc

---
+++ DTO/LICHLAMVIEC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LICHLAMVIEC
+    {
+        public int MaNV { get; set; }
+        public string Ca { get; set; }
+        public DateTime Ngay { get; set; }
+    }
+}
