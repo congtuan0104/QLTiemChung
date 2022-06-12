24e344f8a72820ec9681fd3a2eedab2b889b3591
HuynhNgocVan <vanhuynh2209@gmail.com>
2022-06-12 22:24:18 +0700

    Đăng ký tiêm tiêm chủng

---
+++ DTO/GoiVaccine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class GoiVaccine
+    {
+            public int MaGoiVaccine { get; set; }
+            public string TenGoiVaccine { get; set; }
+            public string MoTa { get; set; }
+            public int SoLuongTon { get; set; }
+            public decimal Gia { get; set; }
+            public bool DuocChon { get; set; }
+    }
+}
---
+++ DTO/Vaccine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Vaccine
+    {
+        public int MaVaccine { get; set; }
+        public string TenVaccine { get; set; }
+        public string NhaSanXuat { get; set; }
+        public string MoTa { get; set; }
+        public int SoLuongTon { get; set; }
+        public decimal Gia { get; set; }
+        public bool DuocChon { get; set; }
+    }
+}
