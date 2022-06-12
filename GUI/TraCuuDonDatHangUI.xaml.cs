93fa9c1e1588c3b7a4fdb294fbc87e9c7534dc61
LÊ HÙNG SƠN <19120349@student.hcmus.edu.vn>
2022-06-12 22:16:19 +0700

    Tra cứu đơn đặt mua vaccine

--- GUI/TraCuuDonDatHangUI.xaml.cs
+++ GUI/TraCuuDonDatHangUI.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DTO;
+using BUS;
 
 namespace GUI
 {
@@ -19,9 +21,47 @@ namespace GUI
     /// </summary>
     public partial class TraCuuDonDatHangUI : Window
     {
+        PhieuDatMua_BUS pdm = new PhieuDatMua_BUS();
         public TraCuuDonDatHangUI()
         {
             InitializeComponent();
+            HienThiDSKH();
+        }
+        private void HienThiDSKH()
+        {
+            // Lấy ds khách hàng đổ vào dgvDSKH
+            List<PhieuDatMua> dsHSKH = pdm.XEMDONDATHANG();
+            dgvDDH.ItemsSource = dsHSKH;
+           dgvDDH.Items.Refresh();
+        }
+
+        private void Button_ClickTKDDH(object sender, RoutedEventArgs e)
+        {
+            string MaKhachhang = txbMaKH.Text;
+           
+            if (MaKhachhang == "" )
+            {
+                MessageBox.Show("Vui lòng nhập thông tin trước khi tìm kiếm");
+                return;
+            }
+
+            // Tra cứu danh sách
+            List<PhieuDatMua> dsHSKH = pdm.TraCuuKH(MaKhachhang);
+
+            if (dsHSKH.Count == 0)
+            {
+                MessageBox.Show("Không tìm thấy thông tin khách hàng");
+            }
+            dgvDDH.ItemsSource = dsHSKH;
+            dgvDDH.Items.Refresh();
+
+        }
+
+        private void btnQuayLai_Click(object sender, RoutedEventArgs e)
+        {
+            MenuUI menuUI = new MenuUI();
+            menuUI.Show();
+            this.Close();
         }
     }
 }
