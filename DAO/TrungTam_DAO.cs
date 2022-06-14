505fe1779fa4c8bc91b4bfbcfc66cb748a234813
HuynhNgocVan <vanhuynh2209@gmail.com>
2022-06-14 23:54:41 +0700

    Tạo danh sách trung tâm

---
+++ DAO/TrungTam_DAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DAO
+{
+    public class TrungTam_DAO : DataAccess
+    {
+        public List<TrungTam> LayDSTrungTam_DB()
+        {
+            // Lấy danh sách tất cả các trung tâm 
+            MoKetNoi();
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT MaTT, TenTT, SDT_TT, DiaChi " +
+                 "FROM TRUNGTAM";
+
+            command.Connection = conec;
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<TrungTam> dsTrungTam = new List<TrungTam>();
+            while (reader.Read())
+            {
+                TrungTam tt = new TrungTam();
+                tt.MaTT = reader.GetInt32(0);
+                tt.TenTT = reader.GetString(1);
+                tt.SDT_TT = reader.GetString(2);
+                tt.DiaChi = reader.GetString(3);
+                dsTrungTam.Add(tt);
+            }
+            reader.Close();
+            return dsTrungTam;
+        }
+    }
+}
